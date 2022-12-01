cb4d3d0411c2418a42ecafb9634345a0cdb4940c
Romain Roffé <rofferom@gmail.com>
2022-12-01 21:49:05 +0100

    Clean day1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,45 +1,27 @@
+const INPUT: &str = include_str!("day1_input.txt");
+
 fn parse_input(input: &str) -> Vec<u32> {
-    let values = {
-        let mut values: Vec<_> = input.lines().map(|s| s.parse::<u32>().ok()).collect();
-
-        values.push(None);
-
-        values
-    };
-
-    let mut out = vec![];
-    let mut current = 0;
-    for v in values {
-        match v {
-            Some(v) => {
-                current += v;
-            }
-            None => {
-                out.push(current);
-                current = 0;
-            }
-        }
-    }
-
-    out
+    input
+        .split("\n\n")
+        .map(|block| block.lines().map(|l| l.parse::<u32>().unwrap()).sum())
+        .collect()
 }
 
 fn solve_part1(input: &str) -> u32 {
     let values = parse_input(input);
-    values.iter().max().copied().unwrap()
+    values.into_iter().max().unwrap()
 }
 
 fn solve_part2(input: &str) -> u32 {
     let mut values = parse_input(input);
 
     values.sort_by_key(|i| std::cmp::Reverse(*i));
-    values.iter().take(3).sum()
+    values.into_iter().take(3).sum()
 }
 
 fn main() {
-    let input = include_str!("day1_input.txt");
-    println!("Part 1: {}", solve_part1(input));
-    println!("Part 2: {}", solve_part2(input));
+    println!("Part 1: {}", solve_part1(INPUT));
+    println!("Part 2: {}", solve_part2(INPUT));
 }
 
 #[cfg(test)]
@@ -63,12 +45,10 @@ mod tests {
 
     #[test]
     fn day1() {
-        let input = include_str!("day1_input.txt");
-
         assert_eq!(solve_part1(TEST_INPUT), 24000);
-        assert_eq!(solve_part1(input), 67016);
+        assert_eq!(solve_part1(INPUT), 67016);
 
         assert_eq!(solve_part2(TEST_INPUT), 45000);
-        assert_eq!(solve_part2(input), 200116);
+        assert_eq!(solve_part2(INPUT), 200116);
     }
 }
